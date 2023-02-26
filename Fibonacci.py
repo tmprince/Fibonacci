22b8fc7ca6a686855d52e93101fbad425d44270e
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-25 18:03:09 -0800

    Fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,17 @@
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+def main():
+    print("The Fibonacci Series for 16")
+    for i in range(16):
+        print(fib(i), end=", ")
+    print("....")
+
+if __name__ == "__main__":
+    main()
 
